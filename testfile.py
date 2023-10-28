3b48de4242f99f5fefa38bfcbc2b648ef63388ad
Ujjwal Singh <ujjwalsingh@ujjwal.singh>
2023-10-28 23:35:38 +0530

    bot-test

--- testfile.py
+++ testfile.py
@@ -2,7 +2,7 @@ try:
   a = int(input())
   b = int(input())
   result = a/b * 1000 + 13 + 8 - 9 + 10 + 1 + 111
-  print(result + 10 + 202 + 3314 + 2221 - 50 - 800)
+  print(result + 10 + 202 + 3314 + 2221 - 50 - 80)
   print("OKAYS")
   print("YAYAAAAA")
 except:
